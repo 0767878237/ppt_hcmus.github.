ef662e5443b0ec1f7497d2a4ff49c96a19932056
Anh Tuan <leducanhtuan01267878237@gmail.com>
2023-12-22 20:30:07 +0700

    Add function files

--- PPchiadoi.m
+++ PPchiadoi.m
@@ -1,32 +1,22 @@
-clear all
-clear
 
-[c,n]=chiadoi(fx,a,b,saiso)
-
-
-function [c,n] =chiadoi(fx, a, b, saiso)
-    fxi=str2func(['@(x)',fx]);
-    n=0;
-    if fxi(a)*fxi(b)>0
-        c=NaN;
-        return
+f = @(x) 3*x^3 - 8*x^2 - 20*x + 16;
+[nghiem,solanlap] = chiadoi(f,0.2,1,0.004)
+function [nghiem,solanlap] = chiadoi(f,a,b,saiso)
+i = 0; 
+if f(a)*f(b) > 0
+    disp('Phuong trinh vo nghiem');
+else
+    c=(a+b)/2;
+while abs(f(c)) > saiso
+    if f(a)*f(c) > 0
+        a=c;
+    else
+        b=c;
     end
-    
-    if fxi(a)==0
-        c=a;
-        return
-    elseif fxi(b)==0
-        c=b;
-        return
-    end
-    
-    while(abs(a-b)>=saiso)
-        c=(a+b)/2;
-        if fxi(a)*fxi(c)<0
-            b=c;
-        else
-            a=c;
-        end
-        n=n+1;
-    end
+    c=(a+b)/2;
+    i = i+1;
+end
+end
+nghiem = c;
+solanlap = i;
 end
--- lap.m
+++ lap.m
@@ -1,16 +1,15 @@
-function [xM,n] = lap(fp,f,a,b,saiso)
-f=inline(f);
-if(f(a)*f(b)>0)
-    disp('Khong co nghiem trong khoang nay');
-else
-    xT=a;
-    n=0;
-    e=saiso+1;
-    while(e>saiso)
-        xM=fp(xT);
-        e=abs(xM-xT);
-        xT=xM;
-        n=n+1;
-    end 
-end
+f = @(x) ((3*x^3 - 8*x^2 + 16)/20);
+[nghiem,solanlap] = PPlap(f,0.2,1,0.004)
+function [nghiem,solanlap] = PPlap(f,a,b,saiso)
+  x(2) = b;
+  x(1) = a;
+  i = 2;
+  for i = i:100000
+      x(i) = f(x(i-1));
+      if abs(x(i) - x(i-1)) < saiso
+          break;
+      end
+  end
+  nghiem = x(i);
+  solanlap = i;
 end
